f99538e9d7d53b6a87e9699c1b71dde1fb62a52e
mal <1881633+akmalkhadir@users.noreply.github.com>
2018-09-27 19:21:05 +0100

    All done.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,11 +1,33 @@
 class Gallery
 
+  @@all = []
+
   attr_reader :name, :city
 
-def initialize(name, city)
-  @name = name
-  @city = city
-end
+  def initialize(name, city)
+    @name = name
+    @city = city
+    @@all << self
+  end
 
+  def self.all
+    @@all
+  end
+
+  def self.cities
+    self.all.map { |g| g.city }.uniq
+  end
+
+  def artists
+    Painting.all.select { |p| p.gallery == self }.map {|p| p.artist }.uniq
+  end
+
+  def artist_names
+    artists.map { |a| a.name }.uniq
+  end
+
+  def total_experience
+    Painting.all.select { |p| p.gallery == self }.sum {|p| p.artist.!!!years_active}
+  end
 
 end
