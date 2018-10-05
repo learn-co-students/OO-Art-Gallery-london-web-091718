1090daae7a9f35a52afd70e596dd113d7ebdea01
wimbleclaw <aisha305@hotmail.co.uk>
2018-10-05 08:58:33 +0100

    Done.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,10 +2,31 @@ class Gallery
 
   attr_reader :name, :city
 
-def initialize(name, city)
-  @name = name
-  @city = city
-end
+  @@all= []
 
+  def initialize(name, city)
+    @city = city
+    @name = name
+    @@all << self
+  end
 
+  def self.all
+    @@all
+  end
+
+  def self.cities
+    self.all.collect {|gallery| gallery.city}.uniq
+  end
+
+  def artist
+    Painting.all.select {|painting| painting.gallery == self}.map {|painting| painting.artist}
+  end
+
+  def artist_name
+    artist.map {|artist| artist.name}
+  end
+
+  def combined_years_of_experience
+    artist.map {|artist| artist.years_active}.inject(&:+)
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,15 @@ def reload
   load 'config/environment.rb'
 end
 
+louise = Artist.new("Louise", 10)
+claire = Artist.new("Claire", 15)
+
+fitz = Gallery.new("Fitzwilliam Mueseum", "Cambridge")
+barbican = Gallery.new("Barbican", "London")
+
+church = Painting.new("A walk through a church", "impressionist", louise, fitz)
+animation = Painting.new("Halloween night", "modern", claire, barbican)
+
+
+
 binding.pry
